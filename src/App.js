241b785b157bc99822f29b13e173c30a7a9d97b5
Prasad Shembekar <prasadmshembekar@gmail.com>
2023-07-25 14:30:44 +0530

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+import { Helmet } from 'react-helmet';
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  const TITLE = 'CryptoSage';
+  return (
+    <BrowserRouter>
+        <Helmet>
+          <title>{ TITLE }</title>
+        </Helmet>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
